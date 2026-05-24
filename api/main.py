d2d6bb028be4ce037a1268b2b32bdfe7840bb75c
VaheMaleryan <vahe_maleryan@edu.aua.am>
2026-05-24 16:46:00 +0400

    chore: remove stale CORS origin

--- api/main.py
+++ api/main.py
@@ -103,7 +103,6 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        "https://vahemaleryan.github.io",
     ],
     allow_credentials=True,
     allow_methods=["*"],
